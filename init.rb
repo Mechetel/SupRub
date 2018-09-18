58b22bd174626962ad28ed2e3d3a39c723fb4960
mechetel <dima.homa5@gmail.com>
2018-09-18 17:31:41 +0300

    updating

--- init.rb
+++ init.rb
@@ -1,6 +1,6 @@
 def hello_world(first_name = "", family_name = "")
   if first_name != "" && family_name != ""
-    puts "My name is " + first_name + " " + family_name
+    puts "My name " + first_name + " " + family_name
   end
 end
 
