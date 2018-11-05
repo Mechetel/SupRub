f48b24f121321b9afe3565eaa04b91ea817c0216
mechetel <dima.homa5@gmail.com>
2018-11-05 16:51:28 +0200

    updating

--- item.rb
+++ item.rb
@@ -14,3 +14,8 @@ item1 = Item.new({ :weight => 100, :price => 30 })
 puts item1.price
 puts item1.weight
 p item1
+
+item2 = Item.new({ :weight => 200, :price => 50 })
+puts item2.price
+puts item2.weight
+p item2
